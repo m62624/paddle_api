430158939c76c51a3bdc2ab77b581a1d6f0883d7
Mansur Azatbek <mansur62624@gmail.com>
2024-11-26 01:49:46 +0500

    chore: move the product structures to the entities folder

--- src/client/get_product.rs
+++ src/client/get_product.rs
@@ -1,38 +1,5 @@
-use crate::entities::product::ProductResponse;
-use serde::Deserialize;
-
 use super::Client;
-
-// https://developer.paddle.com/api-reference/products/get-product#response
-#[derive(Deserialize)]
-#[cfg_attr(any(feature = "debug", feature = "logs", test), derive(Debug))]
-pub struct GetProductResponse {
-    data: ProductResponse,
-    meta: Meta,
-}
-
-// https://developer.paddle.com/api-reference/products/get-product#response
-#[derive(Deserialize)]
-#[cfg_attr(any(feature = "debug", feature = "logs", test), derive(Debug))]
-pub struct Meta {
-    request_id: String,
-}
-
-impl GetProductResponse {
-    pub fn data(&self) -> &ProductResponse {
-        &self.data
-    }
-
-    pub fn meta(&self) -> &Meta {
-        &self.meta
-    }
-}
-
-impl Meta {
-    pub fn request_id(&self) -> &str {
-        &self.request_id
-    }
-}
+use crate::entities::product::GetProductResponse;
 
 impl<'a> Client<'a> {
     /// Get a single product by its ID.
--- src/entities/product.rs
+++ src/entities/product.rs
@@ -5,8 +5,10 @@ use serde::{Deserialize, Serialize};
 #[cfg_attr(any(feature = "debug", feature = "logs", test), derive(Debug))]
 pub struct ProductData {
     name: String,
-    description: Option<String>,
     tax_category: Option<String>,
+    description: Option<String>,
+    #[serde(rename = "type")]
+    p_type: String,
     image_url: Option<String>,
     custom_data: Option<serde_json::Value>,
 }
@@ -27,4 +29,35 @@ pub struct ProductResponse {
     import_meta: Option<serde_json::Value>,
     created_at: String,
     updated_at: String,
+}
+
+// https://developer.paddle.com/api-reference/products/get-product#response
+#[derive(Deserialize)]
+#[cfg_attr(any(feature = "debug", feature = "logs", test), derive(Debug))]
+pub struct GetProductResponse {
+    data: ProductResponse,
+    meta: Meta,
+}
+
+// https://developer.paddle.com/api-reference/products/get-product#response
+#[derive(Deserialize)]
+#[cfg_attr(any(feature = "debug", feature = "logs", test), derive(Debug))]
+pub struct Meta {
+    request_id: String,
+}
+
+impl GetProductResponse {
+    pub fn data(&self) -> &ProductResponse {
+        &self.data
+    }
+
+    pub fn meta(&self) -> &Meta {
+        &self.meta
+    }
+}
+
+impl Meta {
+    pub fn request_id(&self) -> &str {
+        &self.request_id
+    }
 }
